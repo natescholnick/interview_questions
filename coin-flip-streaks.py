998ab523e455aba30a75afbf4e8e46adb4306f06
Nathaniel Scholnick <nate.scholnick@gmail.com>
2021-11-01 16:00:25 -0500

    from max: 10000 flip instances

---
+++ coin-flip-streaks.py
@@ -0,0 +1,43 @@
+# Sent to me by Max:
+# Over 10000 instances, compute the average number of streaks of length 6 or more in 100 coin flips.
+
+import random
+import math
+
+
+def flip(n):
+    """
+    Flip a coin n times and count the number of streaks
+    """
+    last_value = None
+    current_streak = 0
+    streak_count = 0
+    seq = ''
+    for _ in range(n):
+        # values = {HEADS: 0, TAILS: 1}
+        value = math.floor(random.random() + 0.5)
+        if value == last_value:
+            current_streak += 1
+        else:
+            if current_streak >= 6:
+                streak_count += 1
+            current_streak = 1
+        last_value = value
+    if current_streak >= 6:
+        streak_count += 1
+
+    return streak_count
+
+
+def average_sampling(m, n):
+    """
+    Average the results of flip(n) over m instances
+    """
+    sum = 0
+    for _ in range(m):
+        sum += flip(n)
+
+    return sum/m
+
+
+print(average_sampling(10000, 100))
