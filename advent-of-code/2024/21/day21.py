8423dd8c64569f30867d276b4879b8b32c52a06a
Nathaniel Scholnick <nate.scholnick@gmail.com>
2024-12-12 21:35:27 -0500

    AoC file scaffolding script, only 4 years late!

---
+++ advent-of-code/2024/21/day21.py
@@ -0,0 +1,20 @@
+
+import os
+import sys
+from collections import defaultdict, deque, Counter
+import re
+import heapq
+import math
+import bisect
+import numpy as np
+
+cwd = os.getcwd()
+
+with open(f"{cwd}/advent-of-code/2024/21/input.txt") as f:
+    lines = f.read().splitlines()
+
+# Part 1
+res = 0
+
+# Part 2
+res2 = 0
