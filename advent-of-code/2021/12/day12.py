5b180c4695ec51e2625576f1ad375c5116bf82e0
Nathaniel Scholnick <nate.scholnick@gmail.com>
2025-01-22 02:03:43 -0500

    AoC 2021 day 12

--- advent-of-code/2021/12/day12.py
+++ advent-of-code/2021/12/day12.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from collections import defaultdict, deque, Counter
@@ -14,7 +13,38 @@ with open(f"{cwd}/advent-of-code/2021/12/input.txt") as f:
     lines = f.read().splitlines()
 
 # Part 1
-res = 0
+G = defaultdict(list)
+for line in lines:
+    a, b = line.split("-")
+    G[a].append(b)
+    G[b].append(a)
+
+
+def dfs(cave, path):
+    if cave == "end":
+        return 1
+
+    if cave.islower() and cave in path:
+        return 0
+
+    return sum(dfs(nei, path + [cave]) for nei in G[cave])
+
+
+print(dfs("start", []))
+
 
 # Part 2
-res2 = 0
+def dfs2(cave, path, hasRepeat):
+    if cave == "end":
+        return 1
+
+    foundRepeat = hasRepeat
+    if cave.islower() and cave in path:
+        if hasRepeat or cave == "start":
+            return 0
+        foundRepeat = True
+
+    return sum(dfs2(nei, path + [cave], foundRepeat) for nei in G[cave])
+
+
+print(dfs2("start", [], False))
