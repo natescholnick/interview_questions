3ccc64cdbc58c0c59bb75a8c8c234f8a842f5ccc
Nathaniel Scholnick <nate.scholnick@gmail.com>
2025-12-01 01:28:11 -0500

    AoC 2025: day 1

--- advent-of-code/2025/01/day01.py
+++ advent-of-code/2025/01/day01.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from collections import defaultdict, deque, Counter
@@ -6,7 +5,6 @@ import re
 import heapq
 import math
 import bisect
-import numpy as np
 
 cwd = os.getcwd()
 
@@ -14,7 +12,46 @@ with open(f"{cwd}/advent-of-code/2025/01/input.txt") as f:
     lines = f.read().splitlines()
 
 # Part 1
+val = 50
 res = 0
+for line in lines:
+    dir = line[0]
+    add = int(line[1:])
+    if dir == "L":
+        val -= add
+    else:
+        val += add
+
+    val %= 100
+    if not val:
+        res += 1
+
+print(res)
+
 
 # Part 2
+def cross_zero(a, b, mod=100):
+    if b > a:
+        return b // mod
+
+    if b > 0:
+        return 0
+
+    return -((b - 1) // 100) - (a == 0)
+
+
 res2 = 0
+val = 50
+for line in lines:
+    dir = line[0]
+    add = int(line[1:])
+    prev = val
+    if dir == "L":
+        val -= add
+    else:
+        val += add
+
+    res2 += cross_zero(prev, val)
+    val %= 100
+
+print(res2)
