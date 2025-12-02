d71ca72e44c55d92ccb382858dedcce619053f39
Nathaniel Scholnick <nate.scholnick@gmail.com>
2025-12-02 01:09:24 -0500

    AoC 2025: day 2

--- advent-of-code/2025/02/day02.py
+++ advent-of-code/2025/02/day02.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from collections import defaultdict, deque, Counter
@@ -11,10 +10,39 @@ import numpy as np
 cwd = os.getcwd()
 
 with open(f"{cwd}/advent-of-code/2025/02/input.txt") as f:
-    lines = f.read().splitlines()
+    line = f.read()
 
 # Part 1
 res = 0
+ranges = line.split(",")
+for r in ranges:
+    a, b = [int(x) for x in r.split("-")]
+    for val in range(a, b + 1):
+        s = str(val)
+        if len(s) % 2:
+            continue
+        if s[: len(s) // 2] == s[len(s) // 2 :]:
+            res += val
+
+print(res)
 
 # Part 2
 res2 = 0
+
+
+def check_invalid(s):
+    m = len(s)
+    for i in range(m // 2):
+        sub_s = s[: i + 1]
+        if sub_s * (m // len(sub_s)) == s:
+            return int(s)
+    return 0
+
+
+for r in ranges:
+    a, b = [int(x) for x in r.split("-")]
+    for val in range(a, b + 1):
+        s = str(val)
+        res2 += check_invalid(s)
+
+print(res2)
