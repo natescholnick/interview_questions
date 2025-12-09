de4ece6a40f76387604320a470dcae1b15a2f3eb
Nathaniel Scholnick <nate.scholnick@gmail.com>
2025-12-09 01:26:13 -0500

    AoC 2025: day 9 Part 1

--- advent-of-code/2025/09/day09.py
+++ advent-of-code/2025/09/day09.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from collections import defaultdict, deque, Counter
@@ -15,6 +14,18 @@ with open(f"{cwd}/advent-of-code/2025/09/input.txt") as f:
 
 # Part 1
 res = 0
+n = len(lines)
+points = []
+for line in lines:
+    points.append(tuple(int(x) for x in line.split(",")))
+
+for i in range(n - 1):
+    x0, y0 = points[i]
+    for j in range(i + 1, n):
+        x1, y1 = points[j]
+        res = max(res, (abs(x1 - x0) + 1) * (abs(y1 - y0) + 1))
+
+print(res)
 
 # Part 2
 res2 = 0
